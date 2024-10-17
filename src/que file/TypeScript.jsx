65dc642a68592c9a43d93ffabadf923f95772fe2
minhazapon <minhajulabedinapon@gmail.com>
2024-10-17 06:09:41 -0700

    added react file

--- src/que file/TypeScript.jsx
+++ src/que file/TypeScript.jsx
@@ -1,9 +1,37 @@
 
+import { useEffect, useState } from "react";
+import DeatilsT from "./DeatilsT";
+
 
 
 const TypeScript = () => {
+
+    const [ type, setType ] = useState([])
+
+
+    useEffect( () =>{
+
+       fetch('http://localhost:5000/typescriptData')
+       .then( res => res.json())
+       .then( data => setType(data) )
+
+    } , [])
+ 
+
+
     return (
-        <div>
+        <div className=" mt-10 mb-10 ml-10 mr-10 flex justify-center ">
+
+
+            <div className=" grid  md:grid-cols-3 gap-5 ">
+
+                  {
+
+                  type.map( type =>  <DeatilsT type={type} ></DeatilsT> )
+
+                  }
+
+            </div>
             
         </div>
     );
