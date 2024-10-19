5ae880ecd7e80de3a6c19c8d565a44da76218994
minhazapon <minhajulabedinapon@gmail.com>
2024-10-19 05:32:44 -0700

    added graphql file

--- src/que file/GraphQL.jsx
+++ src/que file/GraphQL.jsx
@@ -1,9 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import DetailsGraphql from "./DetailsGraphql";
 
 
 
 const GraphQL = () => {
+
+    const { data: query } = useQuery({
+       
+        queryKey: ['query'],
+        queryFn: async () =>{
+         
+            const res = await fetch('http://localhost:5000/graphqlData')
+            return res.json();
+
+        }
+
+    })
+
+
     return (
-        <div>
+        <div className=" mt-10 mb-10 ml-10 mr-10 flex justify-center ">
+
+
+            <div className=" grid  md:grid-cols-3 gap-5 ">
+                {
+                    query?.map( query => <DetailsGraphql query={query} ></DetailsGraphql> )
+                }
+            </div>
             
         </div>
     ); 
