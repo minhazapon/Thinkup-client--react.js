98f1335950638d6f39eca4ef43b67de7aa4ac8c0
minhazapon <minhajulabedinapon@gmail.com>
2024-10-19 04:08:32 -0700

    added aws file

--- src/que file/AWS.jsx
+++ src/que file/AWS.jsx
@@ -1,10 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
+import DetailsAws from "./DetailsAws";
 
 
 
 
 const AWS = () => {
+
+    const {data: query} = useQuery({
+
+
+            queryKey: ['query'],
+            queryFn: async () =>{
+              
+                const res = await fetch('http://localhost:5000/awsData')
+                return res.json()
+
+            }
+
+    })
+
+
+
+
     return (
-        <div>
+        <div className=" mt-10 ml-10 mr-10 mb-10 flex justify-center ">
+
+
+            <div className=" grid  md:grid-cols-3 gap-5">
+
+
+
+                    {
+
+                       query?.map( query => <DetailsAws query={query} ></DetailsAws> )
+
+                    }
+
+
+
+
+
+
+            </div>
             
         </div> 
     );
